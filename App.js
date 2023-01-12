1ead37c1c4da4010775dc7e0b9199efdf7a931b7
ThuHoang2312 <thu.hoang1223@gmail.com>
2023-01-12 13:57:42 +0700

    add image background

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
 import {StatusBar} from 'expo-status-bar';
 import List from './components/List';
 
 const App = () => {
+  const image = {uri: 'http://placekitten.com/100/300'};
   return (
     <>
       <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <ImageBackground
+            source={image}
+            resizeMode="cover"
+            style={styles.image}
+          >
+            <Text style={styles.text}>Homeless kittens</Text>
+          </ImageBackground>
+        </View>
         <List />
       </SafeAreaView>
       <StatusBar style="auto" />
@@ -16,9 +32,29 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#222222',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    margin: 5,
+  },
+  header: {
+    height: 400,
+    width: '100%',
+    marginBottom: 30,
+  },
+  text: {
+    backgroundColor: '#0000FF',
+    color: 'white',
+    fontSize: 20,
+    padding: 20,
+    position: 'absolute',
+    bottom: 20,
+    alignSelf: 'flex-start',
+    fontWeight: 'bold',
   },
 });
 
